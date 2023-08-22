0917c9049db4ff3aaf20e74d031817282f511db1
LittleBugEmpire <littlebugempire@gmail.com>
2023-08-23 00:20:02 +0530

    block lambda

--- lambdaexpressions/BlockLambda.java
+++ lambdaexpressions/BlockLambda.java
@@ -1,2 +1,27 @@
-package lambdaexpressions;public class BlockLambda {
+package lambdaexpressions;
+
+import java.io.Serializable;
+import java.rmi.Remote;
+
+public class BlockLambda {
+
+    interface Fun{
+        int fact(int n);
+    }
+
+    public static void main(String[] args) {
+        Fun fun = (n)->{
+          //block body
+            int res = 1;
+            for(int i=1; i<=n;i++){
+                res = i*res;
+            }
+            return res;
+        };
+
+        System.out.println(fun.fact(5));
+    }
+
+//    Remote
+
 }

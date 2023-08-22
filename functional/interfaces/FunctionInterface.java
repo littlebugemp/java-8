0c9a23b09cad407960992afcbae332d374d61cb4
LittleBugEmpire <littlebugempire@gmail.com>
2023-08-23 00:22:12 +0530

    function interface

---
+++ functional/interfaces/FunctionInterface.java
@@ -0,0 +1,36 @@
+package functional.interfaces;
+
+import java.util.function.Function;
+
+public class FunctionInterface implements Function<Integer, Double> {
+
+    public static void main(String[] args) {
+        //apply, andThen
+        FunctionInterface fI = new FunctionInterface();
+        System.out.println(fI.andThen(a->a*3).apply(5));
+        System.out.println(fI.apply(5));
+
+
+        Function<Integer,Integer> f = (a)->a/2;
+        System.out.println(f.apply(24));
+
+        //identity
+        Function<Integer, Integer> f2 = Function.identity();
+        System.out.println(f2.apply(23));
+    }
+
+    @Override
+    public Double apply(Integer integer) {
+        return integer/2.0;
+    }
+
+    @Override
+    public <V> Function<V, Double> compose(Function<? super V, ? extends Integer> before) {
+        return Function.super.compose(before);
+    }
+
+    @Override
+    public <V> Function<Integer, V> andThen(Function<? super Double, ? extends V> after) {
+        return Function.super.andThen(after);
+    }
+}
